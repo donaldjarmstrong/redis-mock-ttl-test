a73d418ebfbcfdba96e3b3dfeb8e3682cbfa7f4c
Donald.Armstrong <Donald.Armstrong@target.com>
2014-05-06 07:55:43 -0500

    initial checkin

---
+++ test/redis-mock.expire.test.js
@@ -0,0 +1,35 @@
+var redismock = require("redis-mock"),
+	should = require("should"),
+	events = require("events");
+
+if (process.env['VALID_TESTS']) {
+    redismock = require('redis'); 
+}
+
+describe("expire", function () {
+    it("it should set expire", function (done) {
+		var testKey = '/testKey';
+        var r = redismock.createClient("", "", "");
+
+        // it should put some data
+        r.set(testKey, 'someValue', function (err) {
+        	if (err) {
+        		done(err);
+        	}
+
+        	r.expire(testKey, 600, function(err) {
+        		if (err) {
+        			done(err);
+        		}
+
+        		r.ttl(testKey, function(err, ttl) {
+        			if (err) {
+        				done(err);
+        			}
+
+        			ttl.should.be.less.than(600);
+        		});
+        	});
+        });
+    });
+});
